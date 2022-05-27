3a5def34bda00ea77c96cac35da7b2ad69f16223
nefir2 <vadim.ahtamov@gmail.com>
2022-05-27 18:37:58 +0300

    значки и метод расстанавливающий их. написана коллекция где хранятся значки с выбранными рисунками для таблицы. написан метод который их расставляет в таблице, но скрывает при расстановке

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace MatchingGame
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		// --< автоматически созданный код
+		public Form1()
+		{
+			InitializeComponent();
+			РасстановкаВсехЗначков();
+		}
+		// >--
+
+		//код для работы программы
+
+		/// <summary>
+		/// объект рандом для случайного подбора значка в таблице.
+		/// </summary>
+		Random рндм = new Random();
+		/// <summary>
+		/// коллекция со всеми 16 значками для таблицы.
+		/// </summary>
+		List<string> значки = new List<string>()
+		{
+			"'", "'", "L", "L",
+			"l", "l", "x", "x",
+			"#", "#", "m", "m",
+			"r", "r", "ь", "ь",
+		};
+		/// <summary>
+		/// метод для расстановки всех значков в ячейки таблицы.
+		/// </summary>
+		private void РасстановкаВсехЗначков()
+		{
+			foreach(Control control in tableLayoutPanel1.Controls)
+			{
+				Label iconLabel = control as Label;
+				if (iconLabel != null)
+				{
+					int случ = рндм.Next(значки.Count);
+					iconLabel.Text = значки[случ];
+					iconLabel.ForeColor = iconLabel.BackColor; // <-------
+					значки.RemoveAt(случ);
+				}
+			}
+		}
+	}
 }
